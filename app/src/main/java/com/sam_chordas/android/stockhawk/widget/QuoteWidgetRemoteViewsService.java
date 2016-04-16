0fd9af872e8e659315b3185fa3d94187070ac3b8
kshitij <ikshitijsharma@gmail.com>
2016-04-16 11:27:31 +0530

    Final Project Submission

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetRemoteViewsService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetRemoteViewsService.java
@@ -1,11 +1,9 @@
 package com.sam_chordas.android.stockhawk.widget;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Binder;
-import android.os.Build;
 import android.widget.AdapterView;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
@@ -78,30 +76,13 @@ public class QuoteWidgetRemoteViewsService extends RemoteViewsService {
                 RemoteViews views = new RemoteViews(getPackageName(),
                         R.layout.widget_collection_item);
 
+                views.setTextViewText(R.id.stock_symbol, data.getString(7));
+                views.setTextViewText(R.id.change, data.getString(2));
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
-                    setRemoteContentDescription(views, "description");
-                }
-                views.setTextViewText(R.id.stock_symbol, "formattedDat");
-//                views.setTextViewText(R.id.widget_description, description);
-//                views.setTextViewText(R.id.widget_high_temperature, formattedMaxTemperature);
-//                views.setTextViewText(R.id.widget_low_temperature, formattedMinTemperature);
 
-                final Intent fillInIntent = new Intent();
-//                String locationSetting =
-//                        Utility.getPreferredLocation(DetailWidgetRemoteViewsService.this);
-//                Uri weatherUri = WeatherContract.WeatherEntry.buildWeatherLocationWithDate(
-//                        locationSetting,
-//                        dateInMillis);
-//                fillInIntent.setData(weatherUri);
-//                views.setOnClickFillInIntent(R.id.widget_list_item, fillInIntent);
                 return views;
             }
 
-            @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
-            private void setRemoteContentDescription(RemoteViews views, String description) {
-                // views.setContentDescription(R.id.widget_icon, description);
-            }
 
             @Override
             public RemoteViews getLoadingView() {
